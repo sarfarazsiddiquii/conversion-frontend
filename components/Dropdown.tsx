d872fe6bce9f293c733479955aafd6403368fd53
sarfaraz siddiqui <sarfarazsiddiqui199@gmail.com>
2024-07-30 13:30:59 +0530

    some css

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -3,19 +3,35 @@ import { useState } from 'react';
 const units = ['Meter', 'Kilometer', 'Centimeter', 'Millimeter', 'Micrometer', 'Nanometer', 'Mile', 'Yard', 'Foot', 'Inch', 'Light Year'];
 
 export default function Dropdown({ selectedUnit, setSelectedUnit }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleSelect = (unit) => {
+    setSelectedUnit(unit);
+    setIsOpen(false);
+  };
+
   return (
     <div className="dropdown">
-      <div className="dropdown-list">
-        {units.map((unit) => (
-          <div 
-            key={unit} 
-            onClick={() => setSelectedUnit(unit)}
-            className={`dropdown-item ${selectedUnit === unit ? 'selected' : ''}`}
-          >
-            {unit}
-          </div>
-        ))}
+      <div className="dropdown-selected" onClick={toggleDropdown}>
+        {selectedUnit} <span>&#9660;</span>
       </div>
+      {isOpen && (
+        <div className="dropdown-list">
+          {units.map((unit) => (
+            <div 
+              key={unit} 
+              onClick={() => handleSelect(unit)}
+              className={`dropdown-item ${selectedUnit === unit ? 'selected' : ''}`}
+            >
+              {unit}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
