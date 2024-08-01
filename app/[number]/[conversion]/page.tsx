cb3e740f4f758991ff2f87ef93f85a813f839135
sarfaraz siddiqui <sarfarazsiddiqui199@gmail.com>
2024-08-01 17:02:02 +0530

    addes s

--- app/[number]/[conversion]/page.tsx
+++ app/[number]/[conversion]/page.tsx
@@ -23,7 +23,7 @@ export default function ConversionResult() {
     if (!value || !from || !to) return;
 
     try {
-      const response = await fetch(`https://68.183.117.47/api/convert?value=${value}&from=${from}&to=${to}`);
+      const response = await fetch(`https://test.cm-inch.com/api/convert?value=${value}&from=${from}&to=${to}`);
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
